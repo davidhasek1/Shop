8d84d550d59020aadcd0ba816b9988e369f75d00
David Hasek <hasek.david1@gmail.com>
2021-03-07 21:29:57 +0100

    Cart component add navigatins and footer

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,7 +1,24 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import styled from "styled-components";
+import Navigation from "../components/Navigation";
+import CartNavigation from '../components/Cart/CartNavigation'
+import Footer from "../components/Footer";
 
 const Cart: NextPage = () => {
-  return <p>Cart landing page</p>;
+  return (
+    <div>
+      <Head>
+        <title>Cart</title>
+      </Head>
+      <Navigation />
+      <CartNavigation />
+
+        content here
+
+      <Footer />
+    </div>
+  );
 };
 
 export default Cart;
