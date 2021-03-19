7cd56a48bdc39eb576348a93a32f024bf34e5105
David Hasek <hasek.david1@gmail.com>
2021-03-20 00:27:03 +0100

    Modal responsivity

--- components/Modal/ProductDetail.tsx
+++ components/Modal/ProductDetail.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react"
-import styled from "styled-components"
-import Modal from "./Modal"
-import Image from "next/image"
+import { useState } from "react";
+import styled from "styled-components";
+import { breakpoints } from "../../utils/responsivity";
+import Modal from "./Modal";
+import Image from "next/image";
 
 const ProductDetail = () => {
-  const [isDetailOpen, setIsDetailOpen] = useState(true)
+  const [isDetailOpen, setIsDetailOpen] = useState(true);
   return isDetailOpen ? (
     <Modal closeModal={() => setIsDetailOpen(false)}>
       <ProductWrapper>
@@ -12,45 +13,53 @@ const ProductDetail = () => {
           <Image src="/images/product.jpg" width={500} height={500} />
         </ImageContainer>
         <DetailContent>
-          <h1>Details</h1>
-          <p>
+          <Title>Details</Title>
+          <Text>
             ipsumAliqua exercitation nostrud consequat elit ut. Ullamco sint et
             magna sunt elit laboris irure occaecat. Laboris quis cupidatat
             mollit fugiat aliquip ea vUt anim nulla in culpa. elit et
             reprehenderit occaecat aliquip non ullamco pariatur. Ipsum culpa
             consequat enim adipisicing do consequat fugiat.
-          </p>
-          <h1>Recomandations</h1>
-          <p>
+          </Text>
+          <Title>Recomandations</Title>
+          <Text>
             Et enim nostrud irure ea proident non exercitation sit. Non
             consectetur officia exercitation consectetur aliquip elit in labore
             aliquip ullamco ipsum dolor minim. Exercitation eiusmod ea non elit
             Lorem ea voluptate veniam dolore commodo nisi commodo id excepteur.
             Laboris duis nulla elit non aliqua. Non ad commodo incididunt
-            consectetur esse nisi laboris.
-          </p>
-          
+          </Text>
         </DetailContent>
       </ProductWrapper>
     </Modal>
-  ) : null
-}
+  ) : null;
+};
 
 const ProductWrapper = styled.div`
   display: flex;
   height: 100%;
   align-items: center;
-
-`
+  ${breakpoints("flex-direction", "", [{ 800: "column" }])}
+`;
 const ImageContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
   width: 35%;
   margin: 20px;
-`
+  height: 100%;
+  ${breakpoints("width", "", [{ 800: "90%" }])}
+`;
 const DetailContent = styled.div`
   width: 65%;
   margin: 20px;
-`
+  ${breakpoints("width", "", [{ 800: "90%" }])}
+`;
+const Title = styled.h1`
+  ${breakpoints("font-size", "", [{ 800: "20px" }])}
+`;
+const Text = styled.p`
+  ${breakpoints("font-size", "", [{ 800: "14px" }])}
+`;
 
-export default ProductDetail
+export default ProductDetail;
