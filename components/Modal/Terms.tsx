a1f311a1e75d6a19a6658f027e24be311b222c5c
David Hasek <hasek.david1@gmail.com>
2021-03-27 22:36:53 +0100

    terms privacy basic layout and mocked data

--- components/Modal/Terms.tsx
+++ components/Modal/Terms.tsx
@@ -3,7 +3,7 @@ import { breakpoints } from "../../utils/responsivity";
 import Modal from "./Modal";
 import { useSelector, useDispatch, RootStateOrAny } from "react-redux";
 import { openTermsDialog } from "../../store/actions/handlersActions";
-
+import mock from "../../mock/terms.json";
 const TermsDialog = () => {
   const dispatch = useDispatch();
   const { isTermsOpen } = useSelector(
@@ -12,9 +12,25 @@ const TermsDialog = () => {
 
   return isTermsOpen ? (
     <Modal closeModal={() => dispatch(openTermsDialog(false))}>
-      terms and Conditions
+      <TermsWrapper>
+        <Title>Terms &amp; Conditions</Title>
+        {mock.map((item) => {
+          return (
+            <div key={item.id}>
+              <h1>{item.title}</h1>
+              <p>{item.text}</p>
+            </div>
+          );
+        })}
+      </TermsWrapper>
     </Modal>
   ) : null;
 };
 
+const TermsWrapper = styled.div`
+  padding: 25px;
+`;
+const Title = styled.h1`
+  text-align: center;
+`;
 export default TermsDialog;
