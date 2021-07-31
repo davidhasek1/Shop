9056af04be174b85c0d70491ce814768df240966
David Hasek <hasek.david1@gmail.com>
2021-07-31 23:34:31 +0200

    Blog page add post render without functionalities

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,17 +1,29 @@
 import { NextPage } from 'next'
 import PageHeader from 'components/Header'
+import BlogGrid from 'components/Layout/GridLayout'
+import { mocked_blogposts } from '../../MOCK'
+import BlogPost from 'components/BlogPost'
 
 const Blog: NextPage = () => {
+  const blogPosts = mocked_blogposts.map((post, idx: number) => (
+    <BlogPost
+      imageSource={post.imageUrl}
+      title={post.title}
+      isHomePage={false}
+    />
+  ))
   return (
     <div>
       <PageHeader
         title={'This is Title'}
         headerImage={'/images/creme.jpg'}
         isHomePage={false}
-        ownHeight
+        ownHeight={400}
       >
-        Page header children content with <button>button</button>{' '}
+        TODO: Title and another content for top most post in the list + button
+        (CMS) <button>button</button>
       </PageHeader>
+      <BlogGrid content={blogPosts} />
     </div>
   )
 }
