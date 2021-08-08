fe6939d10dfa82bb7e96d3eaa6234cc3d37f2b1a
David Hasek <hasek.david1@gmail.com>
2021-08-08 23:10:31 +0200

    Fix some map func missing key values

--- components/OurCollection/index.tsx
+++ components/OurCollection/index.tsx
@@ -11,6 +11,7 @@ import GridLyout from 'components/Layout/GridLayout'
 const OurCollection = ({ products }) => {
   const productsRender = products.map((product) => (
     <ProductItem
+      key={product.id}
       imageSource={`${url}${product.Images.formats.small.url}`}
       detailID={product.id}
       title={product.Title}
